c0a8519242bcf2603229e291c467007935263db5
appledix <dxl2010@mail.ru>
2016-10-13 00:56:02 +0300

    Внесены изменения

--- pprint_json.py
+++ pprint_json.py
@@ -1,68 +1,32 @@
+import argparse
 import json
-import sys
-
-def read_console_args():
-    return sys.argv[1:]
+import os
 
 def load_data(filepath):
-    parsed_json = None
-    json_file = open(filepath, 'r')
-    parsed_json = json.loads(json_file.read())
-    json_file.close()                
+    with open(filepath, 'r') as json_file:
+        parsed_json = json.loads(json_file.read())              
     return parsed_json
 
-
 def pretty_print_json(data):
-    data = str(data)
-    pretty_data = ""
-    current_indentation = 0
-    quote_is_open = False
-    tab = " " * 4
-    for symbol in data:
-        if symbol == "{":
-            current_indentation += 1
-            pretty_data += symbol + "\n" + (tab * current_indentation)
-
-        elif symbol == "}":
-            current_indentation -= 1
-            pretty_data += "\n" + (tab * current_indentation) + symbol
-
-        elif symbol == "]":
-            current_indentation -= 1
-            pretty_data += "\n" + (tab * current_indentation) + symbol
-        
-        elif symbol == "[":
-            current_indentation += 1
-            pretty_data += symbol + "\n" + (tab * current_indentation)
-
-        elif symbol == ",":
-            if not quote_is_open:
-                pretty_data += symbol + "\n" + (tab * current_indentation)[:-1]
-            else:
-                pretty_data += symbol
-
-        elif symbol == "'":
-            if quote_is_open:
-                quote_is_open = False
-            else:
-                quote_is_open = True
-            pretty_data += symbol   
-        
-        else:
-            pretty_data += symbol
+    pretty_data = json.dumps(data, sort_keys=True, indent=4, ensure_ascii=False)
     return pretty_data
+
+def filepath_is_valid(filepath):
+    if os.path.isfile(filepath) and filepath.endswith(".json"):
+        return True
+    else:
+        return False
 
 
 if __name__ == '__main__':
-    parameters = read_console_args()
-    try:
-        if len(parameters) != 1:
-            raise Exception
-        filepath = parameters[0]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filepath", help="Расположение JSON файла.", type=str)
+    args = parser.parse_args()
+    filepath = args.filepath
+
+    if filepath_is_valid(filepath):
         data = load_data(filepath)
-    except Exception:
-        print\
-        ("Для работы скрипта необходимо передать ему валидный адрес JSON файла.")
-    else:
         print(pretty_print_json(data))
+    else:
+        print("Некорректные входные данные!")
 
